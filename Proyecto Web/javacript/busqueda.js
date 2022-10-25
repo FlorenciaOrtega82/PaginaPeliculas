ae41bc4395d448872e9b64540b87514329f6e30b
Fernando-J-Aguirre <fernandoj_aguirre@hotmail.com>
2022-10-25 09:32:08 -0300

    Dom, detalle de pelicula js + estilo

--- Proyecto Web/javacript/busqueda.js
+++ Proyecto Web/javacript/busqueda.js
@@ -1,3 +1,5 @@
+//La API utilizada es: OMDb API - The Open Movie Database  --  https://omdbapi.com/
+
 const inputBuscar = document.getElementById('inputBuscar');
 const listaPeli = document.getElementById('listaPeli');
 const btnBuscar = document.getElementById('btnBuscar');
@@ -10,7 +12,6 @@ async function load(pelicula) {
     if (respuesta.ok) mostrar(data.Search);
 }
 
-
 btnBuscar.addEventListener('click', () => {
     let nombre = inputBuscar.value.trim();
     if (nombre) {
@@ -21,9 +22,10 @@ btnBuscar.addEventListener('click', () => {
 })
 
 function mostrar(peliculas) {
+    document.getElementById('listaPeli').classList.remove('lista_Hide')
     listaPeli.innerHTML = ""; //se vacía el contenido para nueva búsqueda
     descripcionPeli.innerHTML = "";
-    for (let i = 0; i <peliculas.length; i++) {
+    for (let i = 0; i < peliculas.length; i++) {
         let divPeli = document.createElement('div');
         divPeli.classList.add('listaPelis');
         divPeli.dataset.id = peliculas[i].imdbID;
@@ -37,7 +39,6 @@ function mostrar(peliculas) {
     loadDetallePelicula();
 }
 
-
 function loadDetallePelicula() {
     let listaPelis = document.querySelectorAll('.listaPelis');
     listaPelis.forEach(peli => {
@@ -50,15 +51,15 @@ function loadDetallePelicula() {
 }
 
 function mostrarPeliDetalle(detalle) {
-    listaPeli.innerHTML = "";
+    document.getElementById('listaPeli').classList.add('lista_Hide')
     descripcionPeli.innerHTML = `
     <div class = "peli_img">
         <img src = "${detalle.Poster}" alt = "Poster película">
     </div>
     <div class = "info_peli">
         <h3 class = "titulo_peli">${detalle.Title}</h3>
-        <p class = "año">Año: ${detalle.Year}</p>
-        <p class = "lanzamiento">Lanzamiento: ${detalle.Released}</p>
+        <p class = "año"><b>Año: </b>${detalle.Year}</p>
+        <p class = "lanzamiento"><b>Lanzamiento: </b>${detalle.Released}</p>
         <p class = "genre"><b>Genre:</b> ${detalle.Genre}</p>
         <p class = "writer"><b>Writer:</b> ${detalle.Writer}</p>
         <p class = "actors"><b>Actors: </b>${detalle.Actors}</p>
